b288cce243dac0b642782a699a1c66b4515a2bf5
aleksandersciubisz <aleksander.sciubisz@gmail.com>
2021-12-14 12:04:58 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// Scroll Up icon
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up')
+  if (this.scrollY >= 250) scrollUp.classList.add('show-scroll')
+  else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+//nav toggler
+document.getElementById('nav1').onclick = function () {
+  document.getElementById('check').checked = false
+}
+
+document.getElementById('nav2').onclick = function () {
+  document.getElementById('check').checked = false
+}
+
+document.getElementById('nav3').onclick = function () {
+  document.getElementById('check').checked = false
+}
+
+document.getElementById('nav4').onclick = function () {
+  document.getElementById('check').checked = false
+}
+
+document.getElementById('nav5').onclick = function () {
+  document.getElementById('check').checked = false
+}
